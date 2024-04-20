6f16b7a430549d359ce23233b24b37b3c71b7772
Sushanta Bhowmick <bhosushanta922@gmail.com>
2024-04-20 21:56:00 +0530

    add properties of cookies

--- backend/controllers/userCtrl.js
+++ backend/controllers/userCtrl.js
@@ -185,6 +185,8 @@ exports.logoutUser=catchAsyncErrors(async(req,res,next)=>{
   res.cookie("token",null,{
     expires:new Date(Date.now()),
     httpOnly:true,
+    secure:true,
+    sameSite:"None"
   })
   res.status(200).json({
     success: true,
--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -5,6 +5,8 @@ const sendToken = (user,statusCode,res,message)=>{
     const options ={
         expires:new Date(Date.now()+90*24*60*60*1000),
         httpOnly:true,
+        secure:true,
+        sameSite:"None"
     }
     res.status(statusCode).cookie("token",token,options).json({
         success:true,
